93304468fc7da59e46526e6d4d3919bf83ebbcef
devyatta <riaz.sadykov@mail.ru>
2020-11-11 10:04:27 +0300

    [console program] результат сравнения чисел

--- tutorial01/ConsoleApp2/Program.cs
+++ tutorial01/ConsoleApp2/Program.cs
@@ -1,16 +1,71 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Numerics;
 
 namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine();
 
-            Console.WriteLine("ЫЫЫ");
+        {
+            Console.WriteLine("введите числo");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("введите второе числo");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+            if (num1 < num2)
+            {
+                Console.WriteLine("первое число меньше второго");
+            }
+            else if (num1 > num2)
+            {
+                Console.WriteLine("первое число больше второго");
+            }
+            else
+            {
+                Console.WriteLine("оба числа равны");
+            }
+            Console.ReadKey();
+            
+            
+           
+          
+                  
+       
+        }
+           
+            
+         
+
+
+
+
+
+        }
+          
+
+            
+           
+            
+            
+            
+                
+            
+
+            
+            
+
+
+            
+            
+            
+        
+           
+
 
         } 
-    }
-}
+    
+
