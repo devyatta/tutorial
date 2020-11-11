b0fed35dc4aa6b38a935f8d0b95b40c8156a3532
devyatta <riaz.sadykov@mail.ru>
2020-11-11 13:30:16 +0300

    [console] простенький калькулятор

--- tutorial01/ConsoleApp2/Program.cs
+++ tutorial01/ConsoleApp2/Program.cs
@@ -10,41 +10,58 @@ namespace ConsoleApp2
         static void Main(string[] args)
 
         {
-            Console.WriteLine("введите числo");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("введите второе числo");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            Console.WriteLine($"Введите любое первое число");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Введите любое второе число");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            
+
+            Console.WriteLine("Введите номер операции: 1.Сложение 2.Вычитание 3.Умножение");
+            int name = Convert.ToInt32(Console.ReadLine());
+            switch (name)
+            {
+                case 1:
+                    Console.WriteLine("сложение");
+                    result = number + number2;
+                    break;
+                case 2:
+                    Console.WriteLine("вычитание");
+                    result = number - number2;
+                    break;
+                case 3:
+                    Console.WriteLine("умножение");
+                    result = number * number2;
+                    break;
+                default:
+                    Console.WriteLine("что ты ввел?");
+                    break;
+            }
+
+            Console.WriteLine($"ответ {result}");
 
 
-            if (num1 < num2)
-            {
-                Console.WriteLine("первое число меньше второго");
-            }
-            else if (num1 > num2)
-            {
-                Console.WriteLine("первое число больше второго");
-            }
-            else
-            {
-                Console.WriteLine("оба числа равны");
-            }
-            Console.ReadKey();
-            
-            
-           
-          
-                  
-       
+
+
+
+
+
+
         }
-           
+
             
+        }
+    }
+
          
 
 
 
 
 
-        }
+        
           
 
             
@@ -66,6 +83,6 @@ namespace ConsoleApp2
            
 
 
-        } 
+        
     
 
