bcac17f85e54fcdd682b109e880325ab052b5c50
devyatta <riaz.sadykov@mail.ru>
2020-11-13 08:11:44 +0300

    [задачка] перебпробовал все что знал , не получилось сорян наставник

--- tutorial01/ConsoleApp2/Program.cs
+++ tutorial01/ConsoleApp2/Program.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.Numerics;
+using System.Text.Json;
 
 namespace ConsoleApp2
 {
@@ -10,50 +11,113 @@ namespace ConsoleApp2
         static void Main(string[] args)
 
         {
-            int result = 0;
-            Console.WriteLine($"Введите любое первое число");
-            int number = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine($"Введите любое второе число");
-            int number2 = Convert.ToInt32(Console.ReadLine());
 
-            
 
-            Console.WriteLine("Введите номер операции: 1.Сложение 2.Вычитание 3.Умножение");
-            int name = Convert.ToInt32(Console.ReadLine());
-            switch (name)
+
+
+            // написать программу , запрос х(любое число) запрос второго числа, если сумма предыдущих чисел меньше второго числа , введите заного второе число , если верно то суммировать 
+            // int a . int b  int sum
+            // 1 ЧИСЛО 10 второе 11 то все верно будет 21 , и ввод 2 рого числа должен повториться , с основным условие , т.е. число должно быть больше чем сумма пред чисел
+
+
+            Console.WriteLine("введите число");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.WriteLine("введитие второе число");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = num + num2;
+            Console.WriteLine($"результат {sum}");
+
+            while (true)
             {
-                case 1:
-                    Console.WriteLine("сложение");
-                    result = number + number2;
-                    break;
-                case 2:
-                    Console.WriteLine("вычитание");
-                    result = number - number2;
-                    break;
-                case 3:
-                    Console.WriteLine("умножение");
-                    result = number * number2;
-                    break;
-                default:
-                    Console.WriteLine("что ты ввел?");
-                    break;
+
+                Console.WriteLine("введите следуюющее число");
+
+                int num3 = Convert.ToInt32(Console.ReadLine());
+
+
+                while (num3 <= sum || num3 < sum)
+                {
+
+                    Console.WriteLine("не соответствует требованию");
+
+                    num3 = Convert.ToInt32(Console.ReadLine());
+                }
+
+                int result = sum + num3;
+                Console.WriteLine($"сумма двух предыдущих чисел {result}");
             }
+            
 
-            Console.WriteLine($"ответ {result}");
-
-
-
-
-
-
-
-
+                
+                
+            
         }
 
             
+
+
+              
+            
+
+
+
+
         }
-    }
+
+                
+            
+
+
+
+
+
+
+
+
+
+            
+
+
+             
+                
+            }
+        
+
+            
+
+
+
+        
+
+
+
+    
+
+
+            
+
+
+                 
+
+
+
+
+
+
+
+
+        
+
+            
+        
+   
+    
+    
 
          
 
