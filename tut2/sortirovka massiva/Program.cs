56813ec03105a21694579b87afdff5190eac1ff6
devyatta <riaz.sadykov@mail.ru>
2020-11-17 10:21:23 +0300

    [массивы] иная программа для сортировки массивов , с помощью нахождения минимального числа

--- tut2/sortirovka massiva/Program.cs
+++ tut2/sortirovka massiva/Program.cs
@@ -6,38 +6,47 @@ namespace sortirovka_massiva
     {
         static void Main(string[] args)
         {
-            while (true)
+
+
+            // ввод чисел
+            int[] num = new int[3];
+            Console.WriteLine("введите числа");
+
+            for(int i = 0; i<num.Length;i++)
             {
-                // Ввод чисел
-                int[] nums = new int[7];
-                Console.WriteLine("введите семь чисел");
-                for (int i = 0; i < nums.Length; i++)
-                {
-                    Console.Write("{0}-e число:", i + 1);
-                    nums[i] = Int32.Parse(Console.ReadLine());
+                Console.Write("{0} число: ", i+1);
+                num[i] = Int32.Parse(Console.ReadLine());
+            }
 
-                }
-                // сортировка
 
-                int temp;
-                for (int i = 0; i < nums.Length; i++)
-                {
-                    for (int j = i + 1; j < nums.Length; j++)
 
-                        if (nums[i] > nums[j])
-                        {
-                            temp = nums[i];
-                            nums[i] = nums[j];
-                            nums[j] = temp;
-                        }
-                }
-                // вывод
-                Console.WriteLine("Вывод отсартированного массива");
-                for (int i = 0; i < nums.Length; i++)
+            // сортировка
+            int c;
+            for (int i = num.Length-1; i>=1; i--)
+            {
+               
+
+                for (int j = i-1; j>=0 ; j--)
                 {
-                    Console.WriteLine(nums[i]);
+                    
+                  if(num[j]<num[i])
+                    {
+                        c = num[j];
+                        num[j] = num[i];
+                        num[i] = c;
+                        
+                    }
+
                 }
             }
-        }
+            // вывод
+            Console.WriteLine("числа по возрастанию в обратно порядке");
+            for(int i = 0; i<num.Length;i++)
+            {
+                Console.WriteLine( num[i]);
+
+            }
+
+        }      
     }
 }
