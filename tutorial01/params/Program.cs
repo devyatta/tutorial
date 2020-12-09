58194ed8f55a6bb4c9056ebc5e297bc74c249a59
devyatta <riaz.sadykov@mail.ru>
2020-12-09 11:40:18 +0300

    [zadacha1] сделанно с 1 минусом , не понял как сделать 3 повторения , но думаю это связанно либо с массивом либо с for

---
+++ tutorial01/params/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace kek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           // я не разобрался как сделать 3 повтора , а потом что бы программа выключалась :(
+            while (true)
+            {
+
+
+                Console.WriteLine("введите любое число");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+                if (number <= 5)
+                {
+                    switch (number)
+                    {
+
+                        case (1):
+                            Console.WriteLine("Ю-ху");
+                            break;
+
+                        case (2):
+                            Console.WriteLine("Ю-ху");
+                            break;
+
+                        case (3):
+                            Console.WriteLine("Ю-ху");
+                            break;
+
+                        case (4):
+                            Console.WriteLine("Ю-ху");
+                            break;
+
+                        case (5):
+                            Console.WriteLine("Ю-ху");
+                            break;
+
+                    }
+                }
+
+
+
+
+                else if (number == 6 || number == 7)
+                {
+                    switch (number)
+                    {
+                        case (6):
+                            Console.WriteLine("=(");
+                            break;
+
+                        case (7):
+                            Console.WriteLine("=(");
+                            break;
+                    }
+
+                }
+
+                else
+                {
+
+                    switch (number)
+                    {
+                        default:
+                            Console.WriteLine("попробуй снова");
+                            break;
+                    }
+
+                }
+
+            }
+
+
+            
+
+
+
+        }
+
+    }
+            
+            
+            
+}
+
+                 
+
+
+
+
+
